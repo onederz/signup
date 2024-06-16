2d90feb7c6b19c1b19756d8be0fb19f204fb65d5
onederz <153823430+onederz@users.noreply.github.com>
2024-06-16 15:05:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbwPHcUmaGggLmWq9I4v3X1Xe0tfSvqIAlRM_HLhMEm7ncd3fBDQ9tVM99_UG_F01wcXNA/exec'
+ const scriptURL = 'https://script.google.com/macros/s/AKfycbwPHcUmaGggLmWq9I4v3X1Xe0tfSvqIAlRM_HLhMEm7ncd3fBDQ9tVM99_UG_F01wcXNA/exec'
 
   const form = document.forms['contact-form']
   
@@ -6,6 +6,20 @@
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => alert("Thank you! your form is submitted successfully." ))
+    .then()
     .then(() => { window.location.reload(); })
     .catch(error => console.error('Error!', error.message))
   })
+
+  $(document).ready(function(){
+    $("button").click(function(){
+      $("#submit").hide();
+    });
+  });
+
+  $(function() {
+    $('#select').change(function(){
+      $('.hidden_div').hide();
+      $('.' + $(this).val()).show();
+    });
+  });
